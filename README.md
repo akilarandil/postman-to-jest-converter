793796a27bf8d37fddd6352ee8d4a074bb794502
Akila R Hettiarachchi <akilarandil@gmail.com>
2020-03-21 11:13:55 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# postman-to-jest-converter
+# postman-to-jest-
+
+* Run the following to setup the project
+
+    `yarn install`
+
+* Run the following to convert postman json to jest files
+
+    `node src/init.js <pathToPostmanJson>`
+    
+All generated files will be saved to `tests` folder under the postman collection's name
---
+++ src/init.js
@@ -0,0 +1,117 @@
+const fs = require('fs');
+const beautify = require('js-beautify').js
+const filePath = process.argv[2]
+const path = require('path');
+const collectionAbsPath = path.resolve(filePath);
+const collection = require(collectionAbsPath)
+
+function createVariableFile(rootdir) {
+    const content = `const state = {}
+module.exports = {
+    environment: {
+        set: (key, value) => {
+            state[key] = value
+        },
+        get: (key) => state[key]
+    }
+}`
+    fs.writeFile(`${rootdir}/pm.js`, content, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+}
+
+const createTestFileContent = (absolutePath, describe, test, requestConfig, allTests) => `const axios = require('axios')
+const assert = require('chai').assert
+const pm = require('${absolutePath}/pm.js')
+
+describe('${describe}', () => {
+    it('${test} ....',async () => {
+        const requestConfig = ${JSON.stringify(requestConfig)}
+        const response = await axios.request(requestConfig)
+        const responseBody = JSON.stringify(response.data)
+        const responseCode = {
+            code: response.status
+        }
+        ${allTests}
+    })
+})
+`
+
+const rootDir = `tests/${collection.info.name}`
+makeDirectory(rootDir)
+createVariableFile(rootDir)
+collection.item.forEach((item, index) => {
+    const currentDirectory = `${rootDir}/${++index}. ${item.name}`
+    makeDirectory(currentDirectory)
+    const absolutePath = path.resolve(rootDir);
+    execute(item.item, currentDirectory, absolutePath)
+})
+
+function execute(items, currentDirectory, absolutePath) {
+    items.forEach((item, index) => {
+        if (item._postman_isSubFolder) {
+            const newDir = `${currentDirectory}/${++index}. ${item.name}`
+            makeDirectory(newDir)
+            execute(item.item, newDir, absolutePath)
+        } else {
+            createTests(item, currentDirectory, absolutePath)
+        }
+    })
+}
+
+require('axios')
+
+function createTests({request, event, name}, currentDirectory, absolutePath) {
+    const requestConfig = {
+        url: request.url,
+        method: request.method,
+        data: request.body.raw
+    }
+    const {script = {}} = event.find(e => e.listen === 'test') || {}
+    const allTests = createTest(script.exec)
+    const fileContent = createTestFileContent(absolutePath, 'some', 'test', requestConfig, allTests)
+    createTestFile(name, fileContent, currentDirectory)
+}
+
+function createTest(execs) {
+    if (!execs) return ''
+    let testString = ''
+    const testRegex = /tests\[.*\]\s*=\s*.*/g
+    for (let exec of execs) {
+        if (testRegex.test(exec)) {
+            const convertedTest = convert(exec)
+            testString = `${testString}\n ${convertedTest}`
+        } else {
+            testString = `${testString}\n ${exec}`
+        }
+    }
+    return testString
+}
+
+function convert(string) {
+    const experession = string.match(/\[(.*?)\]/)[1]
+
+    const assertion = string.match(/=(.+)/)[1]
+
+    return `assert(${assertion}, ${experession})`
+}
+
+function createTestFile(name, content, currentDirectory) {
+    const beautyfiedData = beautify(content, { indent_size: 2, space_in_empty_paren: true })
+    fs.writeFile(`${currentDirectory}/${name}.spec.js`, beautyfiedData, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+}
+
+
+function makeDirectory(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
